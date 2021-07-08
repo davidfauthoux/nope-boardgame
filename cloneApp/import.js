337a4d156ca9754da081eca6649b4a4e70b66110
JonathanArsac <jonathanarsac@orange.fr>
2021-07-08 09:18:52 +0200

    Prettier

--- cloneApp/import.js
+++ cloneApp/import.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 var bodyParser = require("body-parser");
 
 const app = express();
@@ -6,31 +6,30 @@ const port = 3000;
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-})
+});
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(`<form method="post" action="/">
     <label>Url git : </label>
     <input type="text" id="path" name="path" required>
-  </form>`)
-})
+  </form>`);
+});
 
 /**
-app.get('/import', (req, res) => {
+ app.get('/import', (req, res) => {
   res.redirect("http://localhost:8086/boardgame/cloneApp/import.html");
 })**/
 
 // parse the body
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const shell = require('shelljs');
-const path = './';
+const shell = require("shelljs");
+const path = "./";
 // Access the parse results as request.body
-app.post('/', function(request, response){
+app.post("/", function (request, response) {
   console.log(request.body.path);
 
-    // go to the path and clone the project
-   shell.cd(path);
-   shell.exec('git clone '+ request.body.path);
-
+  // go to the path and clone the project
+  shell.cd(path);
+  shell.exec("git clone " + request.body.path);
 });
