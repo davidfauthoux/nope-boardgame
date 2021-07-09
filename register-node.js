22d52083a8efe4c1e224875cf67a8fb3fd0df7f0
JonathanArsac <jonathanarsac@orange.fr>
2021-07-09 09:52:50 +0200

    Clone git project with event urlGit

--- register-node.js
+++ register-node.js
@@ -6,15 +6,18 @@
 import * as async from "../modules/async.js";
 import { Server, history } from "../modules/server.js";
 
+const shell = require("shelljs");
+const pathToClone = "../users/boardgame/register";
+
 let args = {};
 for (let a of process.argv) {
-	if (a.startsWith("--")) {
-		a = a.substring("--".length);
-		let i = a.indexOf('=');
-		let k = a.substring(0, i);
-		let v = a.substring(i + 1);
-		args[k] = v;
-	}
+  if (a.startsWith("--")) {
+    a = a.substring("--".length);
+    let i = a.indexOf("=");
+    let k = a.substring(0, i);
+    let v = a.substring(i + 1);
+    args[k] = v;
+  }
 }
 Server.BASE = args.base; // "http://localhost:8086";
 
@@ -22,18 +25,24 @@ console.log("Server.BASE", Server.BASE);
 
 let platform = "boardgame";
 let superuserUserId = "users/" + platform + "/register";
-let server = new Server("/" + superuserUserId);
+let server = new Server("/" + superuserUserId);  // /users/boardgame/register
 
 async.run([
-	() => async.while_(() => true).do_([
-		history(server),
-		(event) => {
-			console.log("EVENT RECEIVED", event);
-
-			if (event.old !== undefined) {
-				for (let oldEvent of event.old) {
-				}
-			}	
-		},
-	]),	
+  () => async.while_(() => true).do_([
+    history(server),
+    (event) => {
+      console.log("EVENT RECEIVED", Object.keys(event)[0]);
+      switch (Object.keys(event)[0]) {
+        case "old" :
+          for (let oldEvent of event.old) {
+          }
+          break;
+        case "urlGit" :
+          // go to the path and clone the project
+          shell.cd(pathToClone);
+          shell.exec("git clone " + event.urlGit);
+          break;
+      }
+    }
+  ])
 ]);
--- register-web.js
+++ register-web.js
@@ -1,16 +1,18 @@
 // http://localhost:8086/boardgame/register-web.html
 
 import * as async from "../modules/async.js";
-import { Server, history } from "../modules/server.js";
+import { Server } from "../modules/server.js";
 // jquery
 
 let platform = "boardgame";
-let superuserUserId = "users/" + platform + "/register";
+let superuserUserId = "users/" + platform + "/register"; // /users/boardgame/register
 
 $(function() {
-
+	// get the queries in url in a map
 	let windowParams = (function() {
 		let u = window.location.search;
+		//Renvoie un objet Location contenant des informations concernant l'URL actuelle du document et fournit des méthodes pour modifier cette URL.
+		// La partie de l'URL qui suit le symbole « ? », avec ce symbole inclus
 		let i = u.indexOf('?');
 		if (i < 0) {
 			return {};
@@ -38,9 +40,10 @@ $(function() {
 		}
 		return p;
 	})();
-
+	// create server with /users/boardgame/register as base
 	let server = new Server("/" + superuserUserId);
 
+	// stack a json in the server
 	let stack = function(toStack) {
 		console.log("STACKING", toStack);
 		async.run([
@@ -48,24 +51,10 @@ $(function() {
 		]);
 	};
 
-	async.run([
-		() => async.while_(() => true).do_([
-			history(server),
-			(event) => {
-				console.log("EVENT RECEIVED", event);
-	
-				if (event.old !== undefined) {
-					for (let oldEvent of event.old) {
-					}
-				}	
-			},
-		]),	
-	]);
-
 	let input = $("<input>").attr("type", "text");
 	let button = $("<button>").text("Test").on("click", function() {
 		stack({
-			test: input.val()
+			urlGit : input.val()
 		});
 	});
 	$("body").append(input).append(button);
