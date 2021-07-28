4308b59022466e72b0b26e0ce616607360e15e31
JonathanArsac <jonathanarsac@orange.fr>
2021-07-28 14:49:33 +0200

    refactor deposit-web.js

--- apps/deposit/deposit-web.js
+++ apps/deposit/deposit-web.js
@@ -1,46 +1,23 @@
-// http://localhost:8086/boardgame/register-web.html
+// http://localhost:8086/boardgame/apps/deposit/deposit-web.html
 
 import * as async from "../../../modules/async.js";
-import { Server, history, uuid } from "../../../modules/server.js";
-// jquery
+import { Server, uuid } from "../../../modules/server.js";
+
 
 const superuserUserId = "boardgame/apps/data/users/register";
-const pathToPortalIndex = "boardgame/apps/portal/portal-web.html";
 
-// get queries in url in a map
-let windowParams = (function() {
-  let u = window.location.search;
-  // Get part of the URL after « ? », including this symbol
-  let i = u.indexOf("?");
-  if (i < 0) {
-    return {};
-  }
-  u = u.substring(i + 1);
-  let p = {};
-  for (let kv of u.split(/&/g)) {
-    let s = kv.trim().split(/=/g);
-    let key;
-    let value;
-    if (s.length === 1) {
-      key = s;
-      value = "";
-    } else {
-      key = decodeURIComponent(s[0]);
-      value = decodeURIComponent(s[1]);
-    }
-    p[key] = value;
-    // let values = p[key];
-    // if (values === undefined) {
-    // 	values = [];
-    // 	p[key] = values;
-    // }
-    // values.push(value);
-  }
-  return p;
-})();
 // create server
 let server = new Server("/" + superuserUserId);
 
+const inputUrlGit = document.getElementById("inputUrlGit");
+const inputNameGame = document.getElementById("inputNameGame");
+const regexSpecialCharacter = /[*|":<>\[\]{}`\\()';@&$]/gi;
+const regexGitLink = /https:\/\/github.com\/[^;]+.git/gi;
+
+/**
+ * Stack event on the server
+ * @param toStack
+ */
 let stack = function(toStack) {
   console.log("STACKING", toStack);
   async.run([
@@ -48,67 +25,59 @@ let stack = function(toStack) {
   ]);
 };
 
-/*async.run([
-    () => async.while_(() => true).do_([
-        history(server),
-        (event) => {
-            console.log("EVENT RECEIVED", event);
-
-            if (event.old !== undefined) {
-                for (let oldEvent of event.old) {
-                }
-            }
-        },
-    ]),
-]);*/
-
-const inputUrlGit = document.getElementById("inputUrlGit");
-const inputNameGame = document.getElementById("inputNameGame");
-const regexSpecialCharacter = /[*|":<>\[\]{}`\\()';@&$]/gi;
-const regexGitLink = /https:\/\/github.com\/[^;]+.git/gi;
-
-document.getElementById("submitUrlGit").addEventListener("click", () => {
-    if (inputUrlGit.value === "") {
-      alert("Git adress undefined");
-      return;
-    }
-    if (!regexGitLink.test(inputUrlGit.value)) {
-      alert("Git adress wrong format");
-      return;
-    }
-    if (inputNameGame.value === "") {
-      alert("Name of the game undefined");
-      return;
-    }
-    if (regexSpecialCharacter.test(inputNameGame.value)) {
-      alert("Name of the game doesn't contain special character");
-      return;
-    }
-    let today = new Date();
-    const dd = String(today.getDate()).padStart(2, "0");
-    const mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
-    const yyyy = today.getFullYear();
-
-    today = mm + "/" + dd + "/" + yyyy;
-    stack({
-      action: "deposit",
-      id: uuid(),
-      url: inputUrlGit.value,
-      game: inputNameGame.value,
-      date: today
-    });
-    //window.location = window.location.protocol + "//" + window.location.host + "/" + pathToPortalIndex;
-  }
-);
-/*
-function cloneGame(){
+/**
+ * Stack deposit event on the server
+ * @param today
+ */
+function stackDepositEvent(today) {
   stack({
     action: "deposit",
+    id: uuid(),
     url: inputUrlGit.value,
-    game: inputNameGame.value
+    game: inputNameGame.value,
+    date: today
   });
-  window.location = window.location.protocol + "//" + window.location.host + "/" + pathToPortalIndex;
-
 }
 
-document.getElementById("formAddGame").onsubmit = function() {cloneGame()};*/
+/**
+ * Return today's date in "dd/mm/yyyy" format
+ * @returns {string}
+ */
+function getTodayDate() {
+  let today = new Date();
+  const dd = String(today.getDate()).padStart(2, "0");
+  const mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  const yyyy = today.getFullYear();
+
+  return mm + "/" + dd + "/" + yyyy;
+}
+
+/**
+ * Verify url and name value to avoid code injection
+ * Stack a deposit event to clone the game
+ */
+function cloneGame() {
+  if (inputUrlGit.value === "") {
+    alert("Git adress undefined");
+    return;
+  }
+  if (!regexGitLink.test(inputUrlGit.value)) {
+    alert("Git adress wrong format");
+    return;
+  }
+  if (inputNameGame.value === "") {
+    alert("Name of the game undefined");
+    return;
+  }
+  if (regexSpecialCharacter.test(inputNameGame.value)) {
+    alert("Name of the game doesn't contain special character");
+    return;
+  }
+  let today = getTodayDate();
+  stackDepositEvent(today);
+}
+
+document.getElementById("submitUrlGit").addEventListener("click", () => {
+    cloneGame();
+  }
+);
