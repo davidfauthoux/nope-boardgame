e723d5d6f85dd97512b4abc489edff5e6972d229
JonathanArsac <jonathanarsac@orange.fr>
2021-08-02 10:08:15 +0200

    correct bug verify url

--- apps/deposit/deposit-web.js
+++ apps/deposit/deposit-web.js
@@ -1,27 +1,46 @@
 // http://localhost:8086/boardgame/apps/deposit/deposit-web.html
 
 import * as async from "../../../modules/async.js";
-import { Server, uuid } from "../../../modules/server.js";
+import { history, Server, uuid } from "../../../modules/server.js";
+import { EncryptionServer } from "../../../modules/encryption";
 
 
 const superuserUserId = "boardgame/apps/data/users/register";
 
 // create server
 let server = new Server("/" + superuserUserId);
+//let encryptionServer = new EncryptionServer();
 
 const inputUrlGit = document.getElementById("inputUrlGit");
 const inputNameGame = document.getElementById("inputNameGame");
 const regexSpecialCharacter = /[*|":<>\[\]{}`\\()';@&$]/gi;
 const regexGitLink = /https:\/\/github.com\/[^;]+.git/gi;
 
+
 /**
  * Stack event on the server
  * @param toStack
  */
-let stack = function(toStack) {
+function stack(toStack) {
   console.log("STACKING", toStack);
   async.run([
     server.stack(toStack)
+  ]);
+};
+
+
+/**
+ * Stack event on the server
+ * @param toStack
+ */
+function stackEncrypted(toStack) {
+  console.log("STACKING", toStack);
+  let userId = encryptionServer.user.id;
+  async.run([
+    encryptionServer.stack({
+      from: userId,
+      data: toStack
+    })
   ]);
 };
 
@@ -73,7 +92,7 @@ function isGitLink(url) {
  * Stack a deposit event to clone the game
  */
 function cloneGame() {
-  if (isNotEmptyString(inputUrlGit.value)) {
+  if (!isNotEmptyString(inputUrlGit.value)) {
     alert("Git adress undefined");
     return;
   }
@@ -81,7 +100,7 @@ function cloneGame() {
     alert("Git adress wrong format");
     return;
   }
-  if (isNotEmptyString(inputNameGame.value)) {
+  if (!isNotEmptyString(inputNameGame.value)) {
     alert("Name of the game undefined");
     return;
   }
@@ -97,3 +116,41 @@ document.getElementById("submitUrlGit").addEventListener("click", () => {
     cloneGame();
   }
 );
+
+/**
+let params = new URLSearchParams(document.location.search.substring(1));
+let unsecuredId = params.get("u");
+let passwordHash;
+let userId;
+
+if (unsecuredId !== undefined) {
+  let passwordHash;
+  userId = userRoot + unsecuredId;
+  encryptionServer.useVault = false;
+  userData.userId = userId;
+  async._([
+
+    EncryptionServer.hash(unsecuredId),
+    (hash) => passwordHash = hash,
+    async.try_([
+      () => encryptionServer.getPublicKey(userId),
+      (publicKey) => {
+        console.log("USER PUBLIC KEY", publicKey);
+      }
+    ]).catch_((_e) => [
+      encryptionServer.createNewUser(userId, passwordHash, "")
+    ]),
+    () => encryptionServer.loadUser(userId, passwordHash, undefined, undefined)
+  ]);
+}
+async.run([
+  () => async.while_(() => true).do_([
+    history(encryptionServer),
+    (event) => {
+      console.debug("Event : ", event);
+      if (event.old !== undefined) {
+      } else { // live event (after the page is loaded)
+      }
+    }
+  ])
+]);**/
